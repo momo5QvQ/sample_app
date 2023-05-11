7cd5cfda01ccbe098c670c88de0bf976dad04fc7
momo5QvQ <132321554+momo5QvQ@users.noreply.github.com>
2023-05-11 06:36:07 +0000

    [finish]3-8

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,9 +5,12 @@ class ListsController < ApplicationController
   end
 
   def create #1/$2.データを受け取り新規登録するためのインスタンス作成
-    list=List.new(list_params)
-    list.save # 3. データをデータベースに保存するためのsaveメソッド実行
-    redirect_to list_path(list.id) # 4. トップ画面へリダイレクト
+    @list=List.new(list_params)
+    if @list.save 
+      redirect_to list_path(@list.id)
+    else
+      render :new
+    end  
   end
 
   def index
